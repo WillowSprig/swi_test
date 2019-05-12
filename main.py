583e091c56dcca72dee234db07bde7e7bd618098
WillowSprig <tesiamakuch@gmail.com>
2019-05-12 13:47:46 +0200

    remove incorrect elements - apparently working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 
 def recursive(tree):
-    for child in tree:
+    for child in list(tree):
         print(child)
         recursive(child)
 
@@ -12,33 +12,40 @@ def remove_incorrect(tree):
     # checking objects
     for object in tree.findall('object'):
         no_name = object.find('obj_name') is None
-        empty_name = not (object.findtext('obj_name') and object.findtext('obj_name').strip())
+        empty_name = not object.findtext('obj_name')
+                          # and object.findtext('obj_name').strip())
         no_fields = object.find('field') is None
         if no_name or empty_name or no_fields:
             tree.remove(object)
+            continue
         # checking object fields
-        for element in object:
+        print("Name: ", object.findtext('obj_name'))
+        for element in list(object):
             tag = element.tag
-            if not (tag == 'obj_name' or tag == 'field')
+            print(element)
+            if not (tag == 'obj_name' or tag == 'field'):
                 object.remove(element)
+                continue
             # checking fields
+            if tag == 'field':
+                print("Entered checking fields")
+                incomplete_field = (element.find('name') is None
+                                    or element.find('type') is None
+                                    or element.find('value') is None)
+                wrong_type = not (element.findtext('type') == 'string'
+                                  or element.findtext('type') == 'int')
+                if incomplete_field or wrong_type:
+                    object.remove(element)
     return ETree.ElementTree(tree)
-
 
 
 def main():
     with open('input.xml', 'r') as infile:
         tree_root = ETree.fromstringlist(['<imaginaryroot>', infile.read(), '</imaginaryroot>'])
     # tree = ETree.ElementTree(tree_root)
-    recursive(tree_root)
-    print('\n\n')
     new_tree = remove_incorrect(tree_root)
-    print('\n\nNew tree:\n\n')
-    # recursive(new_tree)
-    print(new_tree)
     new_tree.write('output.xml')
+    # with open('output.json', 'w') as file:
+    #     json.dump(new_tree.getroot().attrib, file)
 
 main()
-
-# with open('output.json', 'w') as file:
-#     json.dump(something, file)
